deb66bb469573b3b0d5413e549d4d0f56bee95af
João Silva <73296283+Joaorms46@users.noreply.github.com>
2022-11-14 06:38:19 +0000

    segundo

--- gui_app.py
+++ gui_app.py
@@ -3,6 +3,16 @@ from tkinter import filedialog, Text # is for what?
 import os 
 
 root = tk.Tk() # holds the whole app
+#def addApp():
+
 canvas = tk.Canvas(root,height=700,width=700, bg = "#263")
 canvas.pack()
+
+
+
+openFile = tk.Button(root, text="Open File", padx=10,pady=5, fg="white",bg ="#263d42")
+openFile.pack()
+
+runApps = tk.Button(root,text = "Run File",padx=10,pady=5,fg="yellow",bg="#345")
+runApps.pack()
 root.mainloop()#creates a infinity loop to show our window
